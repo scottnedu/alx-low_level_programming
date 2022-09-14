6bae052285f29b7cd32145a398b16bb6fe8cd819
scottnedu <oyiohachinedujude@gmail.com>
2022-09-14 07:14:49 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * 
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main (void)
+{
+	char c;
+
+	if (c = 'a' && c = 'z')
+	{
+		
+	if (c = 'A' && c = 'Z')
+	{
+
+	}
+		return (1);
+
+	else
+	{
+		return (0);
+	}
+}
+
+}
