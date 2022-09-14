85b9472e9031a8038cd3302867cdd7ee14863dd2
scottnedu <oyiohachinedujude@gmail.com>
2022-09-14 10:50:29 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,32 +11,19 @@ int print_sign(int n)
 {
 
 	
-	if(n > 0)
-	
+	if (n > 0)
 	{
-	
-	if(n = 0)
-	
-	{
-		
-	if(n < 0)
-	{
-	}
-	}
-	}
-
 		putchar('+');
-		putchar('0');
-		putchar('-');
-
-
-		return ('1');
-	else 
-	{
-		return ('0');
+		return (1);
 	}
-
-}
-			
-		
-	
+	else if (n = 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else if(n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+}	
