a1e21b98540f0e5d346630555b810924dc1ea3b5
scottnedu <oyiohachinedujude@gmail.com>
2022-09-15 08:29:57 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,53 +12,27 @@
 void print_to_98(int n)
 
 {
-
 	if (n <= 98)
-
 	{
-
 		for (; n <= 98; n++)
-
-		{
-
+		
 			printf("%d", n);
-
-
-
 			if (n == 98)
-
 				continue;
-
 			printf(", ");
-
 		}
-
 		printf("\n");
-
 	}
-
 	else
-
 	{
-
 		for (; n >= 98; n--)
-
 		{
-
 			printf("%d", n);
-
-
-
 			if (n == 98)
-
 				continue;
-
 			printf(", ");
-
 		}
-
 		printf("\n");
-
 	}
 
 }
