c497983c03d17860c227b3eb5dad6f1dfa956b51
scottnedu <oyiohachinedujude@gmail.com>
2022-10-06 21:22:43 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,48 +1,27 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include "main.h"
 /**
- * _strlen - Finds the length of a string.
- * @str: The string to be measured.
- *
- * Return: The length of the string.
- */
-int _strlen(char *str)
-{
-	int i;
-
-	for(i = 0; str[i]; i++)
-		;
-
-	return (i);
-}
-/**
- * main - multiplies two positive numbers
+ * main - Check the code
  * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: always 0 (Success)
+ * @argv: arguments
+ * Return: Nothing
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	unsigned long int a, b, res; 
-	
-	int len_a, len_b;
-
-	len_a = _strlen(argv[1]);
-	len_b = _strlen(argv[2]);
-
-	for (i = 0; i < )
+	int num1, num2, result;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-
-	res = mul(a, b);
-	printf("%d", res);
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
 }
