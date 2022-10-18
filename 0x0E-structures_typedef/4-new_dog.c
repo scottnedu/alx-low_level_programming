85349c62b68b42df276ea83ba4dcaef6532cefd4
scottnedu <oyiohachinedujude@gmail.com>
2022-10-18 09:28:25 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,21 +1,13 @@
 #include "dog.h"
-
 #include <stdlib.h>
-
 #include <stdio.h>
 
 /**
-
  * new_dog - new dog
-
  * @name: name's dog
-
  * @age: age's dog
-
  * @owner: owner's dog
-
  * Return: newdog
-
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
