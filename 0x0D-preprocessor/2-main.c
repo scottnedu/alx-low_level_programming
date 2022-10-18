410e6411b3e3a5e4ced085295a88a043c979dc5b
scottnedu <oyiohachinedujude@gmail.com>
2022-10-18 14:54:36 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Program compilation starts from the main
+ * Description: this program prints the name of the file it was comipiled from
+ * author: sammykingx
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
